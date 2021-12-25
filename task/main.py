131f71f0635373897539e0b0db5c46267da0d6d0
87878787878799 <savelev.vitaliy2003@mail.ru>
2021-12-25 20:17:18 +0300

    final

--- task/main.py
+++ task/main.py
@@ -1,39 +1,35 @@
 def main():
     print("------start------")
     string_to_check = input()
-    pattern_copy = input()
+    pattern = list(input())
     list_to_check = list(string_to_check)
-    pattern_copy = list(pattern_copy)
-    count = 0
+    pattern_index = 0
     result = False
     for char in list_to_check:
-        if char == pattern_copy[count] or pattern_copy[count] == "?":
+        if char == pattern[pattern_index] or pattern[pattern_index] == "?":
             result = True
-        elif pattern_copy[count] == "*":
+        elif pattern[pattern_index] == "*":
             try:
-                if count == len(pattern_copy) or pattern_copy[count + 1] == "?" or pattern_copy[count + 1] == "*":
+                if pattern_index == len(pattern) or pattern[pattern_index + 1] == "?" or pattern[pattern_index + 1] == "*":
                     result = True
-
                 else:
-                    while pattern_copy[count + 1] != list_to_check[count] and count <= len(list_to_check):
-                        list_to_check.pop(count)
+                    while pattern[pattern_index + 1] != list_to_check[pattern_index] and pattern_index <= len(list_to_check):
+                        list_to_check.pop(pattern_index)
                         result = False
-                    if pattern_copy[count + 1] == list_to_check[count]:
+                    if pattern[pattern_index + 1] == list_to_check[pattern_index]:
                         result = True
-                        pattern_copy.pop(count)
+                        pattern.pop(pattern_index)
             except:
-                result = True
+                # result = True
                 break
-
         else:
             result = False
-
-        if result == False or len(pattern_copy) > len(list_to_check) or (count + 2 > len(pattern_copy) < len(list_to_check)):
+        if result == False or len(pattern) > len(list_to_check) or (pattern_index + 2 > len(pattern) < len(list_to_check)):
             result = False
             break
         # if count + 2 > len(pattern_copy):
         #     break
-        count += 1
+        pattern_index += 1
     print(result)
 
 
